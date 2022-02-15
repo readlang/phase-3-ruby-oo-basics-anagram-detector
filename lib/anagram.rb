0c1b90abd1ac2e47f6223aa78cb8336f1fcf8bc3
Read Langworthy <rlangworthy@gmail.com>
2022-02-14 17:03:19 -0800

    commit tree 96989bb0eda3cb11f6af9107b1e71c0c47283174

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require "pry"
+
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word        
+    end
+
+    def match(array)
+        new_array = array.select do |current|
+            @word.chars.sort == current.chars.sort
+        end
+        new_array  
+    end
+
+end
+
+
+
+#binding.pry
